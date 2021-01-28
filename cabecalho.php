a61e2ab491224ade8b767809dc42d043e3e5e711
Déborah Rodrigues <deborahrodriguesn@hotmail.com>
2021-01-28 16:54:34 -0300

    atualização de arquivos cabecalho,formContato,style

--- cabecalho.php
+++ cabecalho.php
@@ -6,20 +6,23 @@ include'formContato.php';
         <title>Formulário de Contato</title>
         <link rel="stylesheet" type="text/css" href="style.css">
     </head>
-    
+
     <body>
         <div class="titulo-contato">
             <h1>Por favor informe seus dados </h1>
             <h2>Para receber promoções</h2>
         </div>
         <div class="formulario-contato">
-            <form id="formulario-contato" method="post" action="<?= $_SERVER["PHP_SELF"];?>">
-                <input name="nome"  type="text"class="formulario-controle" placeholder="Nome Completo"><br><br>
-                <input name="email"  type="text"class="formulario-controle" placeholder="E-mail"><br><br>
-                <input name="telefone"  type="text"class="formulario-controle" placeholder="Whatsapp (91)9999-9999"><br><br>
+            <form id="formulario-contato" method="post" action="<?= $_SERVER["PHP_SELF"]; ?>">
+                <input name="nome"  type="text"class="formulario-controle" placeholder="Nome Completo" value="<?= $nome ?>"><br><span class="error"><?= $nome_error ?></span><br>
+
+                <input name="email"  type="text"class="formulario-controle" placeholder="E-mail" value="<?= $email ?>"><br><span class="error"><?= $email_error ?></span><br>
+
+                <input name="telefone"  type="text"class="formulario-controle" placeholder="Whatsapp (91)9999-9999" value="<?= $telefone ?>"><br> <span class="error"><?= $telefone_error ?></span><br>
+
                 <input name="submit"  type="submit" class="formulario-controle submit" value="Enviar">
             </form>
         </div>
-        
+
     </body>
 </html>
